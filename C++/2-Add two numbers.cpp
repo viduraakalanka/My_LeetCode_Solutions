33f61fbe3d6cab3c24bde6719128ccdd8ffdf57e
Vidura Akalanka <37825080+viduraakalanka@users.noreply.github.com>
2022-06-09 15:32:29 +0530

    Create 2-Add two numbers.cpp

---
+++ C++/2-Add two numbers.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        ListNode* temp;
+        ListNode headNode(0);
+        temp = &headNode;
+        int c = 0;
+        while (c || l1  || l2 ){
+            c += (l1?l1->val:0)+(l2?l2->val:0);
+            
+            temp->next = new ListNode(c%10);
+            temp = temp->next;
+            c = c/10;
+            
+            if (l1) l1 = l1->next;
+        
+            if (l2) l2 = l2->next;
+        
+        }
+        return headNode.next;
+    }
+};
